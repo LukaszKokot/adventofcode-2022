096739d73827c1a5184a652144a3c0ea5fb01c37
Lukasz Kokot <lkokot@kumojin.com>
2022-12-20 08:59:16 -0500

    Add second part of advent 03

--- src/advent.003.spec.ts
+++ src/advent.003.spec.ts
@@ -1,4 +1,7 @@
-import { computeSumOfPriorities } from './advent.003';
+import {
+  computeSumOfGroupPriorities,
+  computeSumOfPriorities,
+} from './advent.003';
 
 describe('Advent 02', () => {
   describe('computeSumOfPriorities', () => {
@@ -39,6 +42,40 @@ describe('Advent 02', () => {
             inputFilePath: `${__dirname}/test-data/advent.003/all-rucksacks.txt`,
           })
         ).resolves.toMatchInlineSnapshot(`8401`);
+      });
+    });
+  });
+
+  describe('computeSumOfGroupPriorities', () => {
+    describe('On an empty file', () => {
+      it('should return 0', async () => {
+        await expect(
+          computeSumOfGroupPriorities({
+            inputFilePath: `${__dirname}/test-data/empty.txt`,
+          })
+        ).resolves.toBe(0);
+      });
+    });
+
+    describe('On a file with many rucksacks', () => {
+      it('should return the sum of group priorities', async () => {
+        await expect(
+          computeSumOfGroupPriorities({
+            inputFilePath: `${__dirname}/test-data/advent.003/many-rucksacks.txt`,
+            groupLength: 3,
+          })
+        ).resolves.toBe(70);
+      });
+    });
+
+    describe('On a file with all rucksacks', () => {
+      it('should return the sum of group priorities', async () => {
+        await expect(
+          computeSumOfGroupPriorities({
+            inputFilePath: `${__dirname}/test-data/advent.003/all-rucksacks.txt`,
+            groupLength: 3,
+          })
+        ).resolves.toMatchInlineSnapshot(`2641`);
       });
     });
   });
--- src/advent.003.ts
+++ src/advent.003.ts
@@ -1,6 +1,25 @@
 import { open } from 'node:fs/promises';
 
-const findFirstMatchingCharacterOnBothSidesOfString = (
+const findAllMatchingCharacterInAllStrings = (
+  strings: string[]
+): RegExpMatchArray | null => {
+  let currentMatches: RegExpMatchArray[] | null;
+  let allMatches: string | null = null;
+
+  strings.forEach(s => {
+    if (allMatches === null) {
+      allMatches = s;
+      return;
+    }
+
+    currentMatches = [...s.matchAll(new RegExp(`([${allMatches}])`, 'g'))];
+    allMatches = currentMatches.map(m => m[0]).join('');
+  });
+
+  return allMatches;
+};
+
+const findAllMatchingCharacterOnBothSidesOfString = (
   s: string
 ): RegExpMatchArray | null => {
   const l = s.substring(0, s.length / 2);
@@ -28,10 +47,40 @@ export const computeSumOfPriorities = async ({
   let priorities = 0;
 
   for await (line of file.readLines()) {
-    matches = findFirstMatchingCharacterOnBothSidesOfString(line);
+    matches = findAllMatchingCharacterOnBothSidesOfString(line);
     if (matches?.length) {
       priorities += getPriorityOfCharacter(matches[0]);
     }
   }
+  return priorities;
+};
+
+export const computeSumOfGroupPriorities = async ({
+  inputFilePath,
+  groupLength = 3,
+}: {
+  inputFilePath: string;
+  groupLength?: number;
+}): Promise<number | undefined> => {
+  const file = await open(inputFilePath);
+  let matches: RegExpMatchArray | null;
+  let line: string;
+  let lines: string[] = [];
+  let priorities = 0;
+
+  for await (line of file.readLines()) {
+    if (lines.length < groupLength) {
+      lines.push(line);
+    }
+
+    if (lines.length === groupLength) {
+      matches = findAllMatchingCharacterInAllStrings(lines);
+      if (matches?.length) {
+        priorities += getPriorityOfCharacter(matches[0]);
+      }
+      lines = [];
+    }
+  }
+
   return priorities;
 };
