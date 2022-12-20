a94ebcc8d965f119ff5d337f27aa7e119c1732d3
Lukasz Kokot <lkokot@kumojin.com>
2022-12-20 15:08:07 -0500

    Add second part of advent 04

--- src/advent.004.spec.ts
+++ src/advent.004.spec.ts
@@ -1,4 +1,4 @@
-import { countFullyContainingSets } from './advent.004';
+import { countFullyContainingSets, countOverlappingSets } from './advent.004';
 
 describe('Advent 04', () => {
   describe('countFullyContainingSets', () => {
@@ -49,6 +49,58 @@ describe('Advent 04', () => {
             inputFilePath: `${__dirname}/test-data/advent.004/all-sections.txt`,
           })
         ).resolves.toMatchInlineSnapshot(`424`);
+      });
+    });
+  });
+
+  describe('countOverlappingSets', () => {
+    describe('On an empty file', () => {
+      it('should return 0', async () => {
+        await expect(
+          countOverlappingSets({
+            inputFilePath: `${__dirname}/test-data/empty.txt`,
+          })
+        ).resolves.toBe(0);
+      });
+    });
+
+    describe('On a file with no fully containing sets', () => {
+      it('should return 0', async () => {
+        await expect(
+          countOverlappingSets({
+            inputFilePath: `${__dirname}/test-data/advent.004/one-not-overlapping-section.txt`,
+          })
+        ).resolves.toBe(0);
+      });
+    });
+
+    describe('On a file with a fully containing set', () => {
+      it('should return 1', async () => {
+        await expect(
+          countOverlappingSets({
+            inputFilePath: `${__dirname}/test-data/advent.004/one-fully-containing-section.txt`,
+          })
+        ).resolves.toBe(1);
+      });
+    });
+
+    describe('On a file with many sections', () => {
+      it('should return a positive value', async () => {
+        await expect(
+          countOverlappingSets({
+            inputFilePath: `${__dirname}/test-data/advent.004/many-sections.txt`,
+          })
+        ).resolves.toBe(4);
+      });
+    });
+
+    describe('On a file with all sections', () => {
+      it('should return a positive value', async () => {
+        await expect(
+          countOverlappingSets({
+            inputFilePath: `${__dirname}/test-data/advent.004/all-sections.txt`,
+          })
+        ).resolves.toMatchInlineSnapshot(`804`);
       });
     });
   });
--- src/advent.004.ts
+++ src/advent.004.ts
@@ -1,10 +1,28 @@
 import { open } from 'node:fs/promises';
 
-const hasFullyContainingSet = (set1: string, set2: string): boolean => {
+type RangeSets = [bigint, bigint][];
+
+const getRangeSets = (set1: string, set2: string): RangeSets => {
   const [low1, high1] = set1.split('-').map(s => BigInt(s));
   const [low2, high2] = set2.split('-').map(s => BigInt(s));
+  return [
+    [low1, high1],
+    [low2, high2],
+  ];
+};
+
+const hasFullyContainingSet = (rangeSets: RangeSets): boolean => {
+  const [low1, high1] = rangeSets[0];
+  const [low2, high2] = rangeSets[1];
 
   return (low1 >= low2 && high1 <= high2) || (low2 >= low1 && high2 <= high1);
+};
+
+const hasOverlappingSet = (rangeSets: RangeSets): boolean => {
+  const [low1, high1] = rangeSets[0];
+  const [low2, high2] = rangeSets[1];
+
+  return (low1 >= low2 && low1 <= high2) || (high1 >= low2 && high1 <= high2);
 };
 
 export const countFullyContainingSets = async ({
@@ -18,7 +36,28 @@ export const countFullyContainingSets = async ({
 
   for await (line of file.readLines()) {
     const [set1, set2] = line.split(',');
-    if (hasFullyContainingSet(set1, set2)) {
+    const rangeSets = getRangeSets(set1, set2);
+    if (hasFullyContainingSet(rangeSets)) {
+      sets++;
+    }
+  }
+
+  return sets;
+};
+
+export const countOverlappingSets = async ({
+  inputFilePath,
+}: {
+  inputFilePath: string;
+}): Promise<number | undefined> => {
+  const file = await open(inputFilePath);
+  let line: string;
+  let sets = 0;
+
+  for await (line of file.readLines()) {
+    const [set1, set2] = line.split(',');
+    const rangeSets = getRangeSets(set1, set2);
+    if (hasFullyContainingSet(rangeSets) || hasOverlappingSet(rangeSets)) {
       sets++;
     }
   }
